6e8efe4d4f13dc6ae3d37e185743024b8f7cab44
Tiago Santos <tfsantosbr@gmail.com>
2020-12-25 19:09:41 -0300

    configuração do producer

--- apps/producers/dotnet/KafkaProducer.ConsoleApp/Program.cs
+++ apps/producers/dotnet/KafkaProducer.ConsoleApp/Program.cs
@@ -15,19 +15,36 @@ namespace KafkaProducer.ConsoleApp
             };
 
             string topic = args[0];
-            string message = args[1];
+            string input = null;
+
+            Console.WriteLine("Iniciando produtor de mensagens...");
+            Console.WriteLine($"Tópico: {topic}");
+            Console.WriteLine("Digite a mensagem a ser enviada:");
 
             using var producer = new ProducerBuilder<Null, string>(config).Build();
 
+            while (true)
+            {
+                input = Console.ReadLine();
+
+                if (!string.IsNullOrWhiteSpace(input))
+                {
+                    await SendMessage(producer, topic, input);
+                }
+            }
+        }
+
+        private static async Task SendMessage(IProducer<Null, string> producer, string topic, string message)
+        {
             var result = await producer.ProduceAsync(
                 topic,
                 new Message<Null, string>
                 { Value = message });
 
-            Console.WriteLine($"[Mensagem Enviada] \n" +
-                $"Topico: {topic} \n" +
-                $"Mensagem: {message} \n" +
-                $"Status: {result.Status}");
+            // Console.WriteLine($"[Mensagem Enviada] \n" +
+            //     $"Topico: {topic} \n" +
+            //     $"Mensagem: {message} \n" +
+            //     $"Status: {result.Status}");
         }
     }
 }
